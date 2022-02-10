678b5a01899fbf5568a03b588e8b4e65a2a5595a
carlosfermino <90504219+carlosfermino@users.noreply.github.com>
2022-02-10 10:39:38 -0300

    Inclusao do projeto
    Inclusão do projeto Upload.

---
+++ src/main/java/projeto/upload/Hello_Word_UpLoad.java
@@ -0,0 +1,11 @@
+package projeto.upload;
+
+public class Hello_Word_UpLoad {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word !!!!");
+		System.out.println("Up load foi !!!!");
+
+	}
+
+}
